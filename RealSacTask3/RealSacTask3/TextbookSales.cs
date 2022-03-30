befaa3c29631249f59604ad6bdba6b7800801628
AndrewItalia <102033396+AndrewItalia@users.noreply.github.com>
2022-03-30 14:25:32 +1100

    Add files via upload

---
+++ RealSacTask3/RealSacTask3/TextbookSales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealSacTask3
+{
+    internal class TextbookSales
+    {
+        // Creates a class which gets the values from the CSV file 
+        public string TextbookName { get; set; }
+        public string Subject { get; set; }
+        public float PurchasePrice { get; set; }
+        public string SalePrice { get; set; }
+        public string Rating { get; set; }
+
+    }
+}
