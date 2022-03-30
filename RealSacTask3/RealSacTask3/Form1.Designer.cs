cc333b4c50c8b2fe55b368082fc893822e23e73e
AndrewItalia <102033396+AndrewItalia@users.noreply.github.com>
2022-03-30 14:10:31 +1100

    Add files via upload

---
+++ RealSacTask3/RealSacTask3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RealSacTask3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DropboxFilter = new System.Windows.Forms.ComboBox();
+            this.txtSearchbox = new System.Windows.Forms.TextBox();
+            this.dgvTextSales = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTextSales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DropboxFilter
+            // 
+            this.DropboxFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DropboxFilter.FormattingEnabled = true;
+            this.DropboxFilter.Items.AddRange(new object[] {
+            "Subject",
+            "Textbook",
+            "Rating"});
+            this.DropboxFilter.Location = new System.Drawing.Point(416, 36);
+            this.DropboxFilter.Name = "DropboxFilter";
+            this.DropboxFilter.Size = new System.Drawing.Size(196, 28);
+            this.DropboxFilter.TabIndex = 0;
+            this.DropboxFilter.SelectedIndexChanged += new System.EventHandler(this.DropboxFilter_SelectedIndexChanged);
+            // 
+            // txtSearchbox
+            // 
+            this.txtSearchbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearchbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchbox.Location = new System.Drawing.Point(768, 36);
+            this.txtSearchbox.Name = "txtSearchbox";
+            this.txtSearchbox.Size = new System.Drawing.Size(247, 28);
+            this.txtSearchbox.TabIndex = 1;
+            this.txtSearchbox.TextChanged += new System.EventHandler(this.txtSearchbox_TextChanged);
+            // 
+            // dgvTextSales
+            // 
+            this.dgvTextSales.AllowUserToAddRows = false;
+            this.dgvTextSales.AllowUserToDeleteRows = false;
+            this.dgvTextSales.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvTextSales.BackgroundColor = System.Drawing.Color.DarkGray;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTextSales.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvTextSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTextSales.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvTextSales.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgvTextSales.Location = new System.Drawing.Point(12, 149);
+            this.dgvTextSales.Name = "dgvTextSales";
+            this.dgvTextSales.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTextSales.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvTextSales.RowHeadersWidth = 62;
+            this.dgvTextSales.RowTemplate.Height = 28;
+            this.dgvTextSales.Size = new System.Drawing.Size(1051, 326);
+            this.dgvTextSales.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(326, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Filter by:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::RealSacTask3.Properties.Resources._1200px_Magnifying_glass_icon_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(709, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 487);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTextSales);
+            this.Controls.Add(this.txtSearchbox);
+            this.Controls.Add(this.DropboxFilter);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTextSales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox DropboxFilter;
+        private System.Windows.Forms.TextBox txtSearchbox;
+        private System.Windows.Forms.DataGridView dgvTextSales;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RealSacTask3/RealSacTask3/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealSacTask3
+{
+    public partial class Form1 : Form
+    {
+        List<TextbookSales> TbSales = new List<TextbookSales>();
+        string TBfilter;
+        BindingSource bs = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+            // Calls CSV load function
+            LoadCSVFile();
+            bs.DataSource = TbSales;
+            dgvTextSales.DataSource = bs;
+        }
+
+        private void LoadCSVFile()
+        {
+            // Hard coded the filepath of the data as the layout does not reqiure a choose file button
+            string filepath = @"C:\Demo\Task3_Shop_Data.csv";
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepath).ToList();
+            foreach (string line in lines)
+            {
+                // Split the lines into seperate columns 
+                List<string> fields = line.Split(',').ToList();
+                TextbookSales t = new TextbookSales();
+                // Use the data from the table to classify the datagridview table and display values and columns
+                // Starting from 0
+                t.TextbookName = fields[0];
+                t.Subject = fields[1];
+                t.PurchasePrice = float.Parse(fields[4]);
+                t.SalePrice = fields[5];
+                t.Rating = fields[6];
+                // Adds the data to the datagridveiw table
+                TbSales.Add(t);
+            }
+        }
+        // Creates a search function for when the user selects one of the options in the dropdown combo box
+        private List<TextbookSales> Search(string target, string filter)
+        {
+            List<TextbookSales> results = new List<TextbookSales>();
+            foreach (TextbookSales t in TbSales)
+            {
+                // When the filter is equal to the subject it will add all subjects realated to the string  
+                if (filter == "Subject")
+                {
+                    if (t.Subject.ToLower().Contains(target.ToLower())) { results.Add(t);}
+                }
+                // When the filter is equal to the Textbook it will add all textbooks realated to the string
+                if (filter == "Textbook")
+                {
+                    if (t.TextbookName.ToLower().Contains(target.ToLower())) { results.Add(t);}
+                }
+                // When the filter is equal to the Rating it will add all Ratings realated to the string
+                if (filter == "Rating")
+                {
+                    if (t.Rating.ToLower() == target.ToLower()) results.Add(t);
+                }
+            }
+            return results;
+        }
+
+        private void DropboxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // gets the value from the ComboBox and assigns it to the textbook filter varible
+            TBfilter = DropboxFilter.Text;
+            // if the textbook filter varible is equal to "Rating" it will then call the sort by rating function
+            if (TBfilter == "Rating") SortByRatingSelSort(TbSales);
+            dgvTextSales.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+
+
+        private void SortByRatingSelSort(List<TextbookSales> Sales)
+        {
+            //I used the Sorting and search algoritms powerpoint as refrence for this code
+            //This is a selection sort that includes a search for the rating of the textbooks
+            int min;
+            // the integer is valued as a minimum
+            string temp;
+            // temporary value holder
+            for (int i = 0; i < Sales.Count - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < Sales.Count; j++)
+                {
+
+                    if (int.TryParse(Sales[j].Rating, out int ratingJ))
+                    { 
+               // It works by finding the smallest element in the rating catergory and putting it at the beginning of the list and then repeating that process on the unsorted remainder of the data.
+                      
+                        if (int.TryParse(Sales[min].Rating, out int ratingMin))
+                            // outputs smallest rating
+                        { if (ratingJ < ratingMin) min = j; }
+                        else
+                        {
+                            min = j;
+                        }
+                    }
+                }
+                //Switchs the current element in Sales[j] with the current smallest one in Sales[min]
+                temp = Sales[min].Rating;
+                Sales[min].Rating = Sales[i].Rating;
+                Sales[i].Rating = temp;
+            }
+
+
+        }
+
+        private void txtSearchbox_TextChanged(object sender, EventArgs e)
+        {
+            // sb = SearchBox
+            List<TextbookSales> sb = Search(txtSearchbox.Text, TBfilter);
+            bs.DataSource = sb;
+            dgvTextSales.DataSource = sb;
+            bs.ResetBindings(false);
+        }
+    }
+}
